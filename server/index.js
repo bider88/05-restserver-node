72308ba188f79705b266b33b24fbdf193d9bad64
Irving Didier <bider88@gmail.com>
2018-06-15 15:30:15 -0500

    Creando peticiones get, post, put, delete. Usando body-parser

--- server/index.js
+++ server/index.js
@@ -1,19 +1,42 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const bodyParser = require('body-parser');
 
 // Db connection
 
 
 // Settings 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // Middlewares
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Routes
-app.get('/', (req, res) => {
-    res.json('Hello word')
+app.get('/users', (req, res) => {
+    res.json('get usuarios');
+})
+
+app.post('/users', (req, res) => {
+    
+    const body = req.body;
+    res.json(body)
+})
+
+app.put('/users/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    res.json({
+        id
+    });
+})
+
+app.delete('/users', (req, res) => {
+    res.json('delete usuarios');
 })
 
 // Static Files
@@ -21,5 +44,5 @@ app.get('/', (req, res) => {
 
 // Starting the server
 app.listen(port, () => {
-    console.log(`restserver listen on port 3000`)
+    console.log(`restserver listen on port ${port}`)
 })
